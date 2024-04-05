015c105fe696e0db7b459221e36a473e1af800c0
Karinne <karinnealmeida@id.uff.br>
2024-04-04 21:56:34 -0300

    alteração de arquivos: rotas.js e categorias.js (aplicação de função de listar categorias ao endpoint caategoria - (testado e funcionando)

---
+++ src/controladores/categorias.js
@@ -0,0 +1,13 @@
+const knex = require('../database/conexao');
+
+const listarCategoria = async (req, res) => {
+    try {
+        const categoria = await knex('categorias').returning();
+        return res.status(200).json(categoria);
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: error.message })
+    }
+}
+
+module.exports = {listarCategoria}
--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,8 @@
 const {Router} = require ('express');
+const { listarCategoria } = require('./controladores/categorias');
 
 const rotas = Router();
+
+rotas.get("/categoria", listarCategoria); //teste de servidor - remover ao finalgit r
 
 module.exports = rotas;
