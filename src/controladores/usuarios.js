839fcd2af6901dfc9119ce2f65434132985d650c
Karinne <karinnealmeida@id.uff.br>
2024-04-07 01:02:34 -0300

    implementação de função de cadastrar usuário

---
+++ src/controladores/usuarios.js
@@ -0,0 +1,19 @@
+const knex = require('../database/conexao');
+const bcrypt = require('bcrypt')
+
+const cadastrarUsuario = async (req, res) => {
+    const {nome, email, senha} = req.body;
+
+    try {
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        await knex('usuarios').insert({nome, email, senha: senhaCriptografada});
+
+        return res.status(201).json({mensagem: 'Usuário cadastrado com sucesso.'});
+        
+    } catch (error) {
+        return res.status(500).json({mensagem: error.message});
+    }
+}
+
+module.exports = {cadastrarUsuario}
