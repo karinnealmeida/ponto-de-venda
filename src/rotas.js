6184075f3583189e358731d0c508a25481c9033e
Karinne <karinnealmeida@id.uff.br>
2024-04-06 23:55:33 -0300

    alteração de arquivo: rotas.js (inclusão de endpoint de cadastro de usuário

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,10 @@
 const {Router} = require ('express');
 const { listarCategoria } = require('./controladores/categorias');
+const { cadastrarUsuario } = require('./controladores/usuarios');
 
 const rotas = Router();
 
 rotas.get("/categoria", listarCategoria);
+rotas.post("/usuario", cadastrarUsuario)
 
 module.exports = rotas;
