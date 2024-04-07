3c6905c97f1fcedc1486f4de0acdcdbf5e2ca8f8
Karinne <karinnealmeida@id.uff.br>
2024-04-07 02:01:39 -0300

    alteração de arquivo: rotas.js (inclui validação de email único no endpoint de cadastrar usuario)

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,11 @@
 const {Router} = require ('express');
 const { listarCategoria } = require('./controladores/categorias');
 const { cadastrarUsuario } = require('./controladores/usuarios');
+const { validarEmailUnico } = require('./intermediarios/usuarios');
 
 const rotas = Router();
 
 rotas.get("/categoria", listarCategoria);
-rotas.post("/usuario", cadastrarUsuario)
+rotas.post("/usuario", validarEmailUnico, cadastrarUsuario)
 
 module.exports = rotas;
