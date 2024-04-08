2a291c44217f6b236d12507210035cd3fcafcf60
Karinne <karinnealmeida@id.uff.br>
2024-04-08 15:11:18 -0300

    correção de bug em esquema de usuário

--- src/esquema/esquemaUsuario.js
+++ src/esquema/esquemaUsuario.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 const esquemaUsuario = joi.object({
     nome: joi.string().required().trim().required(),
     email: joi.string().email().trim().required(),
-    senha: joi.any().min(8).trim().required()
+    senha: joi.string().min(6).trim().required()
 });
 
 module.exports = esquemaUsuario;
