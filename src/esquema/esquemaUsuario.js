73e1cbc6f973c1ae0f3f9af93f80d4b9138c01f1
Karinne <karinnealmeida@id.uff.br>
2024-04-08 16:33:23 -0300

    correção de bug em esquema de usuário

--- src/esquema/esquemaUsuario.js
+++ src/esquema/esquemaUsuario.js
@@ -1,9 +1,22 @@
 const joi = require('joi');
 
 const esquemaUsuario = joi.object({
-    nome: joi.string().required().trim().required(),
-    email: joi.string().email().trim().required(),
-    senha: joi.string().min(6).trim().required()
+    nome: joi.string().required().trim().required().messages({
+        'any.required': 'O campo nome é obrigatório.',
+        'string.empty': 'O campo nome é obrigatório.'
+    }),
+    
+    email: joi.string().email().trim().required().messages({
+        'any.required': 'O campo email é obrigatório.',
+        'string.empty': 'O campo email é obrigatório.',
+        'string.email': 'O campo email precisa ter um formato válido'
+        
+    }),
+    senha: joi.string().min(6).trim().required().messages({
+        'any.required': 'O senha nome é obrigatório.',
+        'string.empty': 'O senha nome é obrigatório.',
+        'string.min': 'A senha deve ter no mínimo 6 caraters.'
+    })
 });
 
 module.exports = esquemaUsuario;
