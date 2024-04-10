3b7e2e543a53d20005c15ee9debcd9b245f8cfa0
Karinne <karinnealmeida@id.uff.br>
2024-04-10 01:29:44 -0300

    criação da função de login no arquivo de controlador do usuário

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,5 +1,7 @@
 const knex = require('../database/conexao');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config;
 
 const cadastrarUsuario = async (req, res) => {
     const {nome, email, senha} = req.body;
@@ -16,4 +18,21 @@ const cadastrarUsuario = async (req, res) => {
     }
 }
 
-module.exports = {cadastrarUsuario}
+const login = async (req, res) => {
+    const {email} = req.body;
+
+    try {
+        const usuario = await knex('usuarios').where({ email }).first().returning();
+
+        const token = jwt.sign({id: usuario.id}, process.env.SENHA_JWT, { 'expiresIn': '1d' })
+
+        const {senha: _, ...usuarioLogado} = usuario;
+
+        return res.status(200).json({usuario: usuarioLogado, token})
+    
+    } catch (error) {
+        return res.status(500).json({mensagem: error.message});
+    }
+}
+
+module.exports = {cadastrarUsuario, login}
