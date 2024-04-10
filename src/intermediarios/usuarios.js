0097a3a85a6cce241e8be89b8367b8e1e102bb67
Karinne <karinnealmeida@id.uff.br>
2024-04-10 01:31:56 -0300

    criação de intermediario para validação de email e senha no endpoint de login

--- src/intermediarios/usuarios.js
+++ src/intermediarios/usuarios.js
@@ -1,4 +1,5 @@
-const knex = require("../database/conexao")
+const knex = require("../database/conexao");
+const bcrypt = require('bcrypt');
 
 const validarEmailUnico = async (req, res, next) => {
     try {
@@ -16,4 +17,24 @@ const validarEmailUnico = async (req, res, next) => {
     }
 }
 
-module.exports = {validarEmailUnico}
+const validarLogin = async (req, res, next) => {
+    const {email, senha} = req.body;
+
+    try {
+
+        const usuario = await knex('usuarios').where({email}).first().returning();
+
+        const senhaValida = await bcrypt.compare(senha, usuario.senha);
+
+        if (!usuario || !senhaValida) {
+            return res.status(404).json({mensagem: 'E-mail ou senha incorretos.'});
+        }
+
+        next();
+        
+    } catch (error) {
+        return res.status(500).json({mensagem: error.message})
+    }
+}
+
+module.exports = {validarEmailUnico, validarLogin}
