4cadff3236883d9ab83ed2ac8581988775e77ec5
Karinne <karinnealmeida@id.uff.br>
2024-04-10 01:32:41 -0300

    criação do endpoint de login

--- src/rotas.js
+++ src/rotas.js
@@ -1,13 +1,14 @@
 const {Router} = require ('express');
 const { listarCategoria } = require('./controladores/categorias');
-const { cadastrarUsuario } = require('./controladores/usuarios');
-const { validarEmailUnico } = require('./intermediarios/usuarios');
+const { cadastrarUsuario, login } = require('./controladores/usuarios');
+const { validarEmailUnico, validarLogin } = require('./intermediarios/usuarios');
 const validarCorpoReq = require('./intermediarios/validarCorpoRequisicao');
 const esquemaUsuario = require('./esquema/esquemaUsuario');
 
 const rotas = Router();
 
 rotas.get("/categoria", listarCategoria);
-rotas.post("/usuario", validarCorpoReq(esquemaUsuario), validarEmailUnico, cadastrarUsuario)
+rotas.post("/usuario", validarCorpoReq(esquemaUsuario), validarEmailUnico, cadastrarUsuario);
+rotas.post("/login", validarLogin, login);
 
 module.exports = rotas;
