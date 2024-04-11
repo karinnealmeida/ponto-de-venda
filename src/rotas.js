37c2b1fcdc67f4615bc89965a9837841da977e7e
Karinne <karinnealmeida@id.uff.br>
2024-04-10 23:59:35 -0300

    inclusão de endpoint de login

--- src/rotas.js
+++ src/rotas.js
@@ -4,11 +4,12 @@ const { cadastrarUsuario, login } = require('./controladores/usuarios');
 const { validarEmailUnico, validarLogin } = require('./intermediarios/usuarios');
 const validarCorpoReq = require('./intermediarios/validarCorpoRequisicao');
 const esquemaUsuario = require('./esquema/esquemaUsuario');
+const esquemaLogin = require('./esquema/esquemaLogin');
 
 const rotas = Router();
 
 rotas.get("/categoria", listarCategoria);
 rotas.post("/usuario", validarCorpoReq(esquemaUsuario), validarEmailUnico, cadastrarUsuario);
-rotas.post("/login", validarLogin, login);
+rotas.post("/login",validarCorpoReq(esquemaLogin), validarLogin, login);
 
 module.exports = rotas;
