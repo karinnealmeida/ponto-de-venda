025867c21275026a1b8088ca6e8515d6a5cc655d
Karinne <karinnealmeida@id.uff.br>
2024-04-11 19:40:46 -0300

    correção em esquema de login (inclusão de mensagem de erro para validação de mínimo de caracteres)

--- src/esquema/esquemaLogin.js
+++ src/esquema/esquemaLogin.js
@@ -1,15 +1,16 @@
 const joi = require('joi');
 
 const esquemaLogin = joi.object({
-    email: joi.string().email().trim().required().messages({
+    email: joi.string().trim().email().required().messages({
         'any.required': 'O campo email é obrigatório.',
         'string.empty': 'O campo email é obrigatório.',
         'string.email': 'O campo email precisa ter um formato válido'
         
     }),
-    senha: joi.string().min(6).trim().required().messages({
+    senha: joi.string().trim().min(6).required().messages({
         'any.required': 'O campo senha é obrigatório.',
         'string.empty': 'O campo senha é obrigatório.',
+        'string.min': 'E-mail ou senha incorretos.'
     })
 });
 
