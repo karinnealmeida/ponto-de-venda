0dada628439cb6c06cf216aabf0a9bcd892d9c55
Karinne <karinnealmeida@id.uff.br>
2024-04-11 19:42:12 -0300

    correção gramatical em mensagem de erro e detalhes estéticos do código

--- src/esquema/esquemaUsuario.js
+++ src/esquema/esquemaUsuario.js
@@ -1,22 +1,22 @@
 const joi = require('joi');
 
 const esquemaUsuario = joi.object({
-    nome: joi.string().required().trim().required().messages({
+    nome: joi.string().trim().required().required().messages({
         'any.required': 'O campo nome é obrigatório.',
         'string.empty': 'O campo nome é obrigatório.'
     }),
     
-    email: joi.string().email().trim().required().messages({
+    email: joi.string().trim().email().required().messages({
         'any.required': 'O campo email é obrigatório.',
         'string.empty': 'O campo email é obrigatório.',
         'string.email': 'O campo email precisa ter um formato válido'
         
     }),
-    senha: joi.string().min(6).trim().required().messages({
-        'any.required': 'O senha nome é obrigatório.',
-        'string.empty': 'O senha nome é obrigatório.',
-        'string.min': 'A senha deve ter no mínimo 6 caraters.'
+    senha: joi.string().trim().min(6).required().messages({
+        'any.required': 'O campo senha é obrigatório.',
+        'string.empty': 'O campo senha é obrigatório.',
+        'string.min': 'A senha deve ter, no mínimo, 6 carateres.'
     })
 });
 
-module.exports = esquemaUsuario
+module.exports = esquemaUsuario;
