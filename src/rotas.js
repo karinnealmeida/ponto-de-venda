1fc090afe1fd0e0ce313e91060759b274ae2e66d
Karinne <karinnealmeida@id.uff.br>
2024-04-22 21:58:49 -0300

    inclusão do intermediario de autenticação de login, reorganização de pastas

--- src/rotas.js
+++ src/rotas.js
@@ -1,15 +1,18 @@
 const {Router} = require ('express');
 const { listarCategoria } = require('./controladores/categorias');
 const { cadastrarUsuario, login } = require('./controladores/usuarios');
-const { validarEmailUnico, validarLogin } = require('./intermediarios/usuarios');
+const { validarEmailUnico, validarLogin } = require('./intermediarios/usuarios/usuarios');
 const validarCorpoReq = require('./intermediarios/validarCorpoRequisicao');
 const esquemaUsuario = require('./esquema/esquemaUsuario');
 const esquemaLogin = require('./esquema/esquemaLogin');
+const autenticarUsuarioLogado = require('./intermediarios/usuarios/autenticacao');
 
 const rotas = Router();
 
 rotas.get("/categoria", listarCategoria);
 rotas.post("/usuario", validarCorpoReq(esquemaUsuario), validarEmailUnico, cadastrarUsuario);
 rotas.post("/login",validarCorpoReq(esquemaLogin), validarLogin, login);
+
+rotas.use(autenticarUsuarioLogado())
 
 module.exports = rotas;
